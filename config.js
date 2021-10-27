21e9c08a7ceb1df40f977a0097008def783b8024
lopackhak333 <90701868+lopackhak333@users.noreply.github.com>
2021-10-27 23:47:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = "your bot token";
+exports.TOKEN = "ODkzODE2Njc2Mzc2NzcyNjQ4.YVg9pQ.tAJxpXwYnjclsqov9eMW4zs2LhY";
 
-exports.PREFIX = 'YOUR PREFIX';
+exports.PREFIX = 'u-';
 
-exports.OWNER_ID = "YOUR ACC ID"
+exports.OWNER_ID = "444556376048664597"
