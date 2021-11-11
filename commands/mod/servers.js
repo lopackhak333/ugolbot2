a6bcb407a594a2c80ccc835fae325a8a51ca1ef7
lopackhak333 <90701868+lopackhak333@users.noreply.github.com>
2021-11-11 21:50:25 +0300

    Create servers.js

---
+++ commands/mod/servers.js
@@ -0,0 +1,17 @@
+const { ownerID } = require("../../owner.json")
+
+module.exports = {
+    config: {
+    
+        name: "servers",
+        description: "servers",
+        usage: "servers <user>",
+        aliases: ["servers"]
+       
+    },
+         client.guilds.cache.forEach(guild => {
+         message.channel.send(`${guild.name}`);
+      });
+
+    }
+}
